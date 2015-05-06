b306b8f4aee149015eb5ece4fd393acf894ed744
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2015-05-06 22:50:53 +0800

    Arrange chat history

--- app/helpers/wiser_chat_helper.rb
+++ app/helpers/wiser_chat_helper.rb
@@ -1,5 +1,5 @@
 module WiserChatHelper
-  def wiser_chat(channel)
-    render "wiser_chat/box", channel: channel
+  def wiser_chat(channel, initial_messages = 10)
+    render "wiser_chat/box", channel: channel, initial_messages: initial_messages
   end
 end
